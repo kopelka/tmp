0835acd0514fb533638c11640d9afc440c74aa3a
Kasia <kasiakopel@gmail.com>
2016-11-22 15:21:08 +0100

    add all projects changes

---
+++ slcd_tutorial/Objects/extra.d
@@ -0,0 +1,9 @@
+.\objects\extra.o: extra.c
+.\objects\extra.o: extra.h
+.\objects\extra.o: C:\Keilv5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.3.0\Device\Include\MKL46Z4.h
+.\objects\extra.o: C:\Keilv5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm0plus.h
+.\objects\extra.o: C:\Keilv5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\extra.o: C:\Keilv5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
+.\objects\extra.o: C:\Keilv5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
+.\objects\extra.o: C:\Keilv5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
+.\objects\extra.o: C:\Keilv5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.3.0\Device\Include\system_MKL46Z4.h
